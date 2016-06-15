c5c11faed77c6007e921a2d7d96031699080fb23
Tristan Maat <tm@tlater.net>
2016-06-15 14:16:52 +0100

    Disable load average display on time display

--- emacs/.emacs.d/conf/modeline-info.el
+++ emacs/.emacs.d/conf/modeline-info.el
@@ -7,4 +7,6 @@
 ;; Display the current time
 (setq display-time-24hr-format t)
 (setq display-time-day-and-date t)
+;; Disable the load average
+(setq display-time-default-load-average nil)
 (display-time-mode t)
