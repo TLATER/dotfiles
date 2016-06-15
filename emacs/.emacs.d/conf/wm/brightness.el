b89b272ec9aa1fe3ccb4349fb39fdbd2e37794de
Tristan Maat <tm@tlater.net>
2016-06-15 13:40:05 +0100

    Make exwm loading modular

---
+++ emacs/.emacs.d/conf/wm/brightness.el
@@ -0,0 +1,8 @@
+(defun brightness-change (amount)
+  "Adjust screen brightness relatively using the amount given"
+  (interactive "NChange screen brightness by: ")
+  (let ((brightness-string))
+    (if (> 0 amount)
+        (setq brightness-string (concat (number-to-string amount) "%"))
+      (setq brightness-string (concat "+" (number-to-string amount) "%")))
+    (start-process "brightness" "*Brightness*" "xbacklight" brightness-string)))
