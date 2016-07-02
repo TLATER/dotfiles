024cbea0c306ab35829306aae1a13aa254ccf2c6
Tristan Maat <tm@tlater.net>
2016-07-02 03:18:24 +0100

    Change auto-inserts configuration
    - Improve elisp configuration
    - Add php configuration

--- emacs/.emacs.d/conf/auto-inserts.el
+++ emacs/.emacs.d/conf/auto-inserts.el
@@ -1,16 +1,49 @@
-;; ------------------------------------------------------------
-;; auto-inserts.el
+;;; auto-inserts.el---Auto-insert configuration
 ;;
-;; Emacs auto inserts
-;; ------------------------------------------------------------
+;; Copyright (C) 2016 Tristan Daniel Maat
+;; Author: Tristan Daniel Maat <tm@tlater.net>
+;; Maintainer: Tristan Daniel Maat <tm@tlater.net>
+;; Created: 29 Jun 2016
+;;
+;;; Code:
 
 (eval-after-load 'autoinsert
   '(define-auto-insert '("\\.el\\'" . "Elisp skeleton")
      '(
        "Short description: "
-       ";; ------------------------------------------------------------" \n
-       ";; " (file-name-nondirectory (buffer-file-name)) \n
+       ";;; " (file-name-nondirectory (buffer-file-name)) "---" str \n
        ";;" \n
-       ";; " str \n
-       ";; ------------------------------------------------------------" \n \n
-       > _ \n)))
+       ";; Copyright (C) "
+       (insert (shell-command-to-string
+                "echo -n $(date +%Y)"))
+       " "
+       (insert user-full-name) \n
+       ";; Author: "
+       (insert user-full-name) " <" (insert user-mail-address) ">" \n
+       ";; Maintainer: "
+       (insert user-full-name) " <" (insert user-mail-address) ">" \n
+       ";; Created: "
+       (insert (shell-command-to-string "echo -n $(date +'%d %b %Y')")) \n
+       ";;" \n
+       ";;; Code:" \n \n
+       > _ \n \n
+       ";;; " (file-name-nondirectory (buffer-file-name)) " ends here" \n)))
+
+(eval-after-load 'autoinsert
+  '(define-auto-insert '("\\.php\\'" . "PHP skeleton")
+     '(
+       "Short description: "
+       "<?php" \n
+       "/**" \n
+       > " * " (file-name-nondirectory (buffer-file-name)) \n
+       > " *" \n
+       > " * " str \n
+       > " *" \n
+       > " * @author "
+       (insert user-full-name) " <" (insert user-mail-address) ">" \n
+       > " * @package " \n
+       > " */" \n \n
+       > _ \n
+       "?>")))
+
+;;; auto-inserts.el ends here
