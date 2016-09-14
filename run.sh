0703e5072728176d204b76815f7bb41de7f33f5a
Tristan Maat <tm@tlater.net>
2016-09-14 20:31:28 +0100

    Fix minor code style issues

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 for folder in *; do
-    if [ -d $folder ]; then
+    if [ -d "$folder" ]; then
         read -p "Do you want to stow $folder? (y|n) " -n 1 -r
         echo
         if [[ $REPLY =~ ^[Yy]$ ]]; then
-            stow -t ~ $folder
+            stow -t ~ "$folder"
         fi
     fi
 done
