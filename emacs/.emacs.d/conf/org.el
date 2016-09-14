785dac84b1c1e24a2723e5e783fc32c333234239
Tristan Maat <tm@tlater.net>
2016-09-14 20:28:25 +0100

    Add new org config

--- emacs/.emacs.d/conf/org.el
+++ emacs/.emacs.d/conf/org.el
@@ -7,9 +7,15 @@
 ;; Set up org
 (use-package org
   :init
-  (setq org-agenda-files '("~/Documents/org/coursework.org"))
+  (setq org-agenda-files '("~/Documents/org/coursework.org"
+                           "~/Documents/org/calendar.org"))
   (setq org-default-notes-file '("~/Documents/org/notes.org"))
-  :defines org-add-link-type
+  (setq org-capture-templates
+        '(("c" "Calendar entry"
+           entry (file "~/Documents/org/calendar.org")
+           "* %^{Title}\n  %^T\n  %?\n")))
+
   :commands org-agenda
   :bind
-  ("C-c a" . org-agenda))
+  ("C-c a" . org-agenda)
+  ("C-c c" . org-capture))
