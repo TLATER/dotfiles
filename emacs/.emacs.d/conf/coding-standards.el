c66e8b0fd20fd70c36197da0846a0415e62f6313
Tristan Maat <tm@tlater.net>
2016-11-21 01:13:00 +0000

    Fix tab offset

--- emacs/.emacs.d/conf/coding-standards.el
+++ emacs/.emacs.d/conf/coding-standards.el
@@ -8,6 +8,7 @@
 (setq-default indent-tabs-mode nil)
 (setq tab-width 4)
 (defvaralias 'c-basic-offset 'tab-width)
+(setq-default c-basic-offset 4)
 
 ;; Remove trailing whitespace on save
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
