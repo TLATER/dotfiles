087f41abef7e88605db2cfa2668c49dce6c7250b
Tristan Maat <tm@tlater.net>
2017-03-16 23:09:22 +0000

    Add alias functionality to launch

--- emacs/.emacs.d/conf/wm/launch.el
+++ emacs/.emacs.d/conf/wm/launch.el
@@ -4,16 +4,26 @@
 ;; EXWM application launching functions
 ;; ------------------------------------------------------------
 
+(defvar launch-aliases '(("chromium" . "chromium --start-fullscreen")))
+
 (defun launch-application-interactively ()
   "Prompts the user to launch an application interactively."
   (interactive)
   (let ((application))
     (setq application (read-shell-command "Launch application: "))
-    (launch-application application)))
+
+    ;; For aliases to work, we need to remove trailing spaces
+    (launch-application (replace-regexp-in-string " $" "" application))))
 
 (defun launch-application (command)
   "Launches an application asynchronously."
-  (let ((name))
+
+  (let ((name) (alias))
+    ;; Apply any aliases for the current command
+    (setq alias (cdr (assoc-string command launch-aliases)))
+    (if alias
+        (setq command alias))
+
     (setq command (split-string-and-unquote command))
 
     (setq name (car command))
