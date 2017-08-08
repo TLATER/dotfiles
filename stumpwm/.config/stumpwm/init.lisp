ee4dbb2547845ba20c5997ebbecafba666478ccf
Tristan Maat <tristan.maat@codethink.com>
2017-08-08 18:19:01 +0100

    Add termite

--- stumpwm/.config/stumpwm/init.lisp
+++ stumpwm/.config/stumpwm/init.lisp
@@ -42,6 +42,8 @@
 (define-key *root-map* (kbd "u") "toggle-gaps")
 
 ;;; Keys:
+(define-key *root-map* (kbd "c") "exec termite")
+(define-key *root-map* (kbd "C-c") "exec termite")
 (define-key *root-map* (kbd "e") "exec emacsclient -c -a 'emacs'")
 (define-key *root-map* (kbd "C-e") "exec emacsclient -c -a 'emacs'")
 (define-key *root-map* (kbd ".") "exec rofi -show run")
