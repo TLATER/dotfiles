e37076e0ed6c38b6ca03d65a8519ac82ad9c605e
Tristan Maat <tm@tlater.net>
2017-08-09 23:30:13 +0100

    custom.el: Update custom.el changes

--- emacs/.emacs.d/personal/custom.el
+++ emacs/.emacs.d/personal/custom.el
@@ -17,11 +17,11 @@
     ("pdflatex -interaction nonstopmode -output-directory %o %f" "bibtex %b" "pdflatex -interaction nonstopmode -output-directory %o %f" "pdflatex -interaction nonstopmode -output-directory %o %f")))
  '(package-selected-packages
    (quote
-    (stumpwm-mode pytest company-jedi jedi-core exwm mocha company-tern js2-refactor jdee yaml-mode web-mode scss-mode geiser company-auctex cdlatex auctex json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company helm-ag helm-descbinds helm-projectile helm smex ido-ubiquitous flx-ido zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line operate-on-number move-text magit projectile ov imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major dash crux browse-kill-ring beacon anzu ace-window)))
+    (helm-purpose window-purpose indium stumpwm-mode pytest company-jedi jedi-core exwm mocha company-tern js2-refactor jdee yaml-mode web-mode scss-mode geiser company-auctex cdlatex auctex json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company helm-ag helm-descbinds helm-projectile helm smex ido-ubiquitous flx-ido zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line operate-on-number move-text magit projectile ov imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major dash crux browse-kill-ring beacon anzu ace-window)))
+ '(pytest-global-name "py.test-3")
  '(rainbow-x-colors-major-mode-list
    (quote
-    (emacs-lisp-mode lisp-mode lisp-interaction-mode c-mode c++-mode java-mode)))
- '(pytest-global-name "py.test-3"))
+    (emacs-lisp-mode lisp-mode lisp-interaction-mode c-mode c++-mode java-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
