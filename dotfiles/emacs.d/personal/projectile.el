76bf87277110cf593d85eb8d607aa70151bb87ea
Tristan Maat <tm@tlater.net>
2019-01-23 14:49:22 +0000

    emacs: Set projectile prefix

---
+++ dotfiles/emacs.d/personal/projectile.el
@@ -0,0 +1,29 @@
+;;; global-bindings.el --- Global keybindings -*- lexical-binding: t; -*-
+
+;;   Copyright (C) 2018  Tristan Daniel Maat
+
+;; Author: Tristan Daniel Maat <mbax4tm2@E-C07KI1803.it.manchester.ac.uk>
+;; Keywords: local
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
+;;; projectile.el ends here
