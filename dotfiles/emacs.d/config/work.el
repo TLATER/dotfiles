15ca83925c5d55417c1d10bd6c571f96ada54771
Tristan Maat <tristan.maat@codethink.co.uk>
2019-06-25 10:24:01 +0100

    emacs: Add automatic note creation

---
+++ dotfiles/emacs.d/config/work.el
@@ -0,0 +1,34 @@
+;;; work.el --- Work-specific config                 -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  Tristan Daniël Maat
+
+;; Author: Tristan Daniël Maat <tm@tlater.net>
+;; Keywords: convenience
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(cond ((string-equal (system-name) "ct-lt-214")
+       (progn
+         (defvar log-dir (expand-file-name
+                          "~/Documents/journal/log/"))
+         (find-file (expand-file-name (format-time-string "%Y/notes-%F.org") log-dir)))))
+
+(provide 'work)
+;;; work.el ends here
