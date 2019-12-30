460cb7daf94206347844ed3c7e07fa07b37eeb7c
Tristan Maat <tristan.maat@codethink.co.uk>
2019-12-30 16:05:03 +0000

    zsh: Set up nix shell prompt

--- dotfiles/zsh/theme.zsh
+++ dotfiles/zsh/theme.zsh
@@ -1,1 +1,11 @@
+# Ensure our env isn't setting anything weird first
+autoload -U promptinit && prompt restore
+
 PROMPT="%B%F{cyan}%n%b %(5~|../|)%4~ %(!|#|$)%f "
+
+if [[ "${IN_NIX_SHELL}" == "pure" ]]; then
+    PROMPT="%F{green}[nix-shell]%f ${PROMPT}"
+elif
+    [[ "${IN_NIX_SHELL}" == "impure" ]]; then
+    PROMPT="%F{yellow}[nix-shell]%f ${PROMPT}"
+fi
