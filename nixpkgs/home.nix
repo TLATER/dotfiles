330d2c337bef03988bbffb9bb12d3bcd88e341ff
Tristan Maat <tristan.maat@codethink.co.uk>
2020-01-07 11:14:30 +0000

    home.nix: Add custom systemd user unit for emacs
    This can't currently be done using the `package =` configuration, see
    NixOS/nixpkgs#66706.

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -9,6 +9,23 @@
     (import ./emacs.nix { inherit pkgs; })
   ];
 
+  systemd.user.services = {
+    emacs = {
+      Unit = {
+        Description = "Emacs text editor";
+        Documentation = "info:emacs man:emacs(1) https://gnu.org/software/emacs";
+      };
+      Service = {
+        ExecStart = "${(import ./emacs.nix { inherit pkgs; })}/bin/emacs --fg-daemon";
+        ExecStop = "${(import ./emacs.nix { inherit pkgs; })}/bin/emacsclient --eval \"(kill-emacs)\"";
+        Environment = "SSH_AUTH_SOCK=%t/keyring/ssh";
+      };
+      Install = {
+        WantedBy = ["default.target"];
+      };
+    };
+  };
+
   programs.home-manager.enable = true;
   home.stateVersion = "19.09";
 }
