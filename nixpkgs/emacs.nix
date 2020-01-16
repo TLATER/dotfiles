f80a0ca9eddbff2104402f8e98aa1b269c3bb74f
Tristan Daniël Maat <tristan.maat@codethink.co.uk>
2020-01-16 11:54:01 +0000

    emacs.nix: Un-vendor use-package-list.el

--- nixpkgs/emacs.nix
+++ nixpkgs/emacs.nix
@@ -1,7 +1,25 @@
 { pkgs, ensure ? (f: n: f), basenameOf ? (f: n: f) }:
 
 with pkgs; with emacsPackagesNg;
-  let emacsDistribution = (emacsPackagesNgGen (if hostPlatform.isDarwin then pkgs.emacsMacport else pkgs.emacs));
+
+
+  let
+    use-package-list = stdenv.mkDerivation {
+      pname = "use-package-list";
+      version = "0e148a4f2096bd27a99025bf0639604afd326db9";
+      src = fetchgit {
+        url = "https://github.com/matthewbauer/bauer.git";
+        rev = "0e148a4f2096bd27a99025bf0639604afd326db9";
+        sha256 = "0mn7pb0h8fqc6i182jcp9lp7igxqmh20i1mabgchf8f4ypnqpr5s";
+        fetchSubmodules = false;
+      };
+      installPhase = ''
+      mkdir -p $out/
+      cp site-lisp/use-package-list.el $out/
+      '';
+    };
+
+    emacsDistribution = (emacsPackagesNgGen (if hostPlatform.isDarwin then pkgs.emacsMacport else pkgs.emacs));
       # Include the emacs config directory; we want to include what
       # use-package includes, so we need to be able to read this.
       config = builtins.filterSource
@@ -16,7 +34,7 @@ with pkgs; with emacsPackagesNg;
         HOME=/tmp SCANNING_PACKAGES=true emacs --batch --quick \
               -L ${emacsDistribution.use-package}/share/emacs/site-lisp/elpa/use-package-* \
               -L ${emacsDistribution.bind-key}/share/emacs/site-lisp/elpa/bind-key-* \
-              -l ${ensure ./use-package-list.el "site-lisp/use-package-list.el"} \
+              -l ${use-package-list}/use-package-list.el \
               --eval "(use-package-list \"${config}/init.el\")" \
               > $out
         '';
