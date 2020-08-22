3e99987fef9901aaede66af7f11091ff5ff432c4
Tristan Daniël Maat <tm@tlater.net>
2020-08-22 18:18:21 +0100

    emacs: Switch to the upstream-provided user service
    The only thing the local one did was define an ExecReload, but this
    was buggy anyway. If we use the upstream user service we can
    automatically create an `emacsclient.desktop` file as well, which is
    more useful than a buggy reload.
    
    The latter is still quite cool, if I ever figure out how to make it
    work I'll just push for it upstream.

--- nixpkgs/configurations/emacs.nix
+++ nixpkgs/configurations/emacs.nix
@@ -6,8 +6,6 @@ let
 in
 {
   home.packages = with pkgs; [
-    local-pkgs.emacs
-
     # Spell checks
     aspell
     aspellDicts.en
@@ -25,35 +23,21 @@ in
       # Recompile init files
       SCANNING_PACKAGES=true ${local-pkgs.emacs}/bin/emacs --batch --quick \
         --eval "(byte-recompile-directory user-emacs-directory 0)"
-
-      # Then reload the running emacs config, if any
-      ${pkgs.systemd}/bin/systemctl --user reload emacs.service
     '';
     recursive = true;
     source = ../../dotfiles/emacs.d;
   };
 
-  # We need to replace the normal systemd user service with this one
-  # so that we can properly reload the service when the files are
-  # updated.
-  #
-  # FIXME: A PR would do nicely.
-  systemd.user.services = {
-    emacs = {
-      Unit = {
-        Description = "Emacs: the extensible, self-documenting text editor";
-        Documentation = "info:emacs man:emacs(1) https://gnu.org/software/emacs";
-        X-RestartIfChanged = false;
-      };
-      Service = {
-        ExecReload = "${local-pkgs.emacs}/bin/emacsclient --eval \"(load-file user-init-file)\"";
-        ExecStart = "${pkgs.runtimeShell} -l -c 'exec ${local-pkgs.emacs}/bin/emacs --fg-daemon'";
-        ExecStop = "${local-pkgs.emacs}/bin/emacsclient --eval \"(kill-emacs)\"";
-        Restart = "on-failure";
-      };
-      Install = {
-        WantedBy = [ "default.target" ];
-      };
+  programs.emacs = {
+    enable = true;
+    package = local-pkgs.emacs;
+  };
+
+  services.emacs = {
+    enable = true;
+    client = {
+      enable = true;
+      arguments = ["--no-wait" "--create-frame"];
     };
   };
 }
