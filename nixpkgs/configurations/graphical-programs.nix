8fb56ad132177f0c0c352cdc6bb7015a7501ac0a
Tristan Daniël Maat <tristan.maat@codethink.co.uk>
2021-01-08 18:23:51 +0000

    Remove webmacs

--- nixpkgs/configurations/graphical-programs.nix
+++ nixpkgs/configurations/graphical-programs.nix
@@ -7,17 +7,5 @@ in
 {
   home.packages = with pkgs; [
     local-pkgs.cap
-
-    webmacs
   ];
-
-  xdg.dataFile."applications/webmacs.desktop".text = ''
-    [Desktop Entry]
-    Version=1.0
-    Type=Application
-    Name=webmacs
-    Comment=webmacs - keyboard driven (emacs key bindings) browser, https://webmacs.readthedocs.io/en/latest/
-    TryExec=webmacs
-    Exec=webmacs
-  '';
 }
