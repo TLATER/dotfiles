8e00ec483a283acac0890cc5b076bdfec318b1c3
Tristan Daniël Maat <tm@tlater.net>
2021-02-03 02:54:19 +0000

    Add flake

--- flake.nix
+++ flake.nix
@@ -2,13 +2,46 @@
   description = "tlater's home configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-20.09";
+    home-manager.url = "github:nix-community/home-manager/release-20.09";
+    nurpkgs.url = "github:nix-community/NUR";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = inputs:
+    let
+      make-home = { username ? "tlater", homeDirectory ? "/home/${username}"
+        , modules ? [ ] }: {
+          nixpkgs.overlays = [
+            (final: prev: { local = import ./nixpkgs/pkgs { pkgs = prev; }; })
+            inputs.nurpkgs.overlay
+          ];
+
+          imports = [ ./nixpkgs ] ++ modules;
+        };
+
+    in {
+      homeConfigurations = {
+        yui = make-home {
+          modules = [
+            ./nixpkgs/configurations/graphical-programs
+            ./nixpkgs/configurations/tty-programs
+            ./nixpkgs/configurations/graphical-programs/games.nix
+          ];
+        };
+        ct-lt-02052 = make-home {
+          modules = [
+            ./nixpkgs/configurations/graphical-programs
+            ./nixpkgs/configurations/tty-programs
+            ./nixpkgs/configurations/tty-programs/work.nix
+          ];
+        };
+      };
+    }
     # Set up a "dev shell" that will work on all architectures
-    inputs.flake-utils.lib.eachDefaultSystem (system:
+    // (inputs.flake-utils.lib.eachDefaultSystem (system:
       let pkgs = inputs.nixpkgs.legacyPackages.${system};
-      in { devShell = pkgs.mkShell { buildInputs = with pkgs; [ nixfmt ]; }; });
+      in {
+        devShell = pkgs.mkShell { buildInputs = with pkgs; [ nixfmt ]; };
+      }));
 }
