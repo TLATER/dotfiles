0bb7ae0dbb067974d76b981a754f803746eaf780
Tristan Daniël Maat <tristan.maat@codethink.co.uk>
2021-02-05 20:50:58 +0000

    nixpkgs: Correctly override the work git email address

--- nixpkgs/configurations/tty-programs/work.nix
+++ nixpkgs/configurations/tty-programs/work.nix
@@ -1,8 +1,8 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   imports = [ ./mail/work.nix ];
 
   home.packages = with pkgs; [ local.gauth ];
-  programs.git.userEmail = "tristan.maat@codethink.co.uk";
+  programs.git.userEmail = lib.mkOverride 99 "tristan.maat@codethink.co.uk";
 }
