29fdff6bfc5bd5d7f81b0aab5928d207703253fa
Tristan Daniël Maat <tm@tlater.net>
2021-04-14 23:08:09 +0100

    nix: Expand flake inputs

--- flake.nix
+++ flake.nix
@@ -9,14 +9,15 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs:
+  outputs =
+    { nixpkgs, nixpkgs-unstable, home-manager, nurpkgs, flake-utils, ... }:
     let
       overlays = [
         (final: prev: {
-          unstable = import inputs.nixpkgs-unstable { system = prev.system; };
+          unstable = import nixpkgs-unstable { system = prev.system; };
         })
         (final: prev: { local = import ./nixpkgs/pkgs { pkgs = prev; }; })
-        inputs.nurpkgs.overlay
+        nurpkgs.overlay
       ];
       make-home = { username ? "tlater", homeDirectory ? "/home/${username}"
         , modules ? [ ] }: {
@@ -49,8 +50,8 @@
       };
     }
     # Set up a "dev shell" that will work on all architectures
-    // (inputs.flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import inputs.nixpkgs { inherit overlays system; };
+    // (flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit overlays system; };
       in {
         packages = import ./nixpkgs/pkgs { inherit pkgs; };
         devShell = pkgs.mkShell { buildInputs = with pkgs; [ nixfmt ]; };
