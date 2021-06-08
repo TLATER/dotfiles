80a69da659e42f48df806a9e881d6d9478f35fde
Tristan Daniël Maat <tristan.maat@codethink.co.uk>
2021-06-08 17:11:54 +0100

    gpg: Fix yubikey port on work laptop
    Not sure why the scdaemon doesn't pick up the correct port on this
    laptop. I assume this is because it has built-in smart card
    functionality that confuses the daemon.

--- nixpkgs/configurations/tty-programs/work.nix
+++ nixpkgs/configurations/tty-programs/work.nix
@@ -3,4 +3,5 @@
 {
   home.packages = with pkgs; [ local.gauth ];
   programs.git.userEmail = lib.mkOverride 99 "tristan.maat@codethink.co.uk";
+  programs.gpg.scdaemonSettings.reader-port = "Yubico Yubi";
 }
