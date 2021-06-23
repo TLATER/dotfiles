3bee0fb37db02836d7ee6b13b934bd2b04e3ccb7
Tristan Daniël Maat <tm@tlater.net>
2021-06-23 01:31:40 +0100

    gcs: Make package work with the newest gcs version

--- nixpkgs/pkgs/gcs.nix
+++ nixpkgs/pkgs/gcs.nix
@@ -1,5 +1,5 @@
-{ sources, stdenv, adoptopenjdk-hotspot-bin-15, jre, makeWrapper, wrapGAppsHook
-}:
+{ sources, lib, stdenv, adoptopenjdk-hotspot-bin-15, jre, makeWrapper
+, wrapGAppsHook }:
 
 stdenv.mkDerivation rec {
   inherit (sources.gcs) pname version src;
@@ -11,19 +11,20 @@ stdenv.mkDerivation rec {
   dontInstall = true;
 
   buildPhase = ''
-    mkdir -p out/bootstrap
-    javac -d out/bootstrap -encoding UTF8 ./bundler/bundler/Bundler.java
-    java -cp out/bootstrap bundler.Bundler --unpackaged
+    patchShebangs bundle.sh
+    substituteInPlace bundle.sh --replace /bin/rm rm
+    ./bundle.sh --unpackaged
   '';
 
-  preFixup = ''
+  preFixup = let v = lib.removePrefix "v" version;
+  in ''
     mkdir -p $out/{bin,share}
     cp -r out/dist/modules $out/share/java
 
     makeWrapperArgs+=("''${gappsWrapperArgs[@]}")
     makeWrapper ${jre}/bin/java $out/bin/gcs \
         ''${makeWrapperArgs[@]} \
-          --add-flags "-cp $out/share/java/com.lowagie.text-2.1.7.jar:$out/share/java/com.trollworks.gcs-${version}.jar com.trollworks.gcs.GCS"
+          --add-flags "-cp $out/share/java/com.lowagie.text-2.1.7.jar -jar $out/share/java/com.trollworks.gcs-${v}.jar"
   '';
 
   dontWrapGApps = true;
