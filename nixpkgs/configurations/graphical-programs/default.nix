7a152b8f0c806fce155f6eda175caa0b1aa22b3a
Tristan Daniël Maat <tm@tlater.net>
2021-12-08 15:29:25 +0000

    rofi: Switch to non-Xresources config
    This currently does not yet migrate the theme.

--- nixpkgs/configurations/graphical-programs/default.nix
+++ nixpkgs/configurations/graphical-programs/default.nix
@@ -13,6 +13,17 @@
     local.cap
   ];
 
+  programs.rofi = {
+    enable = true;
+    font = "mono 11";
+    terminal = "${pkgs.alacritty}";
+    extraConfig = {
+      modi = "drun";
+      drun-display-format = "{name}";
+      show-icons = true;
+    };
+  };
+
   xdg.configFile = {
     "alacritty/alacritty.yml".source =
       "${dotroot}/dotfiles/alacritty/alacritty.yml";
