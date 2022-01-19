feedf3f5ca5df1e15a3428dce74ae1ed15a9c060
Tristan Daniël Maat <tristan.maat@codethink.co.uk>
2022-01-19 12:19:47 +0000

    ssh: Use `VerifyHostKeyDNS` for work ssh servers that support it
    This may be rolled out to other services in the future, keep an eye on
    it.

--- nixpkgs/configurations/tty-programs/work.nix
+++ nixpkgs/configurations/tty-programs/work.nix
@@ -4,4 +4,21 @@
   home.packages = with pkgs; [ local.gauth ];
   programs.git.userEmail = lib.mkOverride 99 "tristan.maat@codethink.co.uk";
   programs.gpg.scdaemonSettings.reader-port = "Yubico Yubi";
+
+  programs.ssh = {
+    matchBlocks = {
+      "shell.codethink.co.uk" = lib.hm.dag.entryAfter [ "*" ] {
+        user = "tristanmaat";
+        extraOptions = { "VerifyHostKeyDNS" = "yes"; };
+      };
+      "gitlab.codethink.co.uk" = lib.hm.dag.entryAfter [ "*" ] {
+        user = "git";
+        extraOptions = { "VerifyHostKeyDNS" = "yes"; };
+      };
+      "git.codethink.co.uk" = lib.hm.dag.entryAfter [ "*" ] {
+        user = "git";
+        extraOptions = { "VerifyHostKeyDNS" = "yes"; };
+      };
+    };
+  };
 }
