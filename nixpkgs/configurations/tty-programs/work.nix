ded78535fb1b189db783764d733c29a257f93423
Tristan Daniël Maat <tristan.maat@codethink.co.uk>
2022-10-28 11:12:55 +0100

    work.nix: Fix yubikey detection
    Seems the scdaemon can now detect the yubikey without specifying it
    explicitly, and in fact fails when we do specify it.

--- nixpkgs/configurations/tty-programs/work.nix
+++ nixpkgs/configurations/tty-programs/work.nix
@@ -5,7 +5,6 @@
 }: {
   home.packages = with pkgs; [local.gauth];
   programs.git.userEmail = lib.mkOverride 99 "tristan.maat@codethink.co.uk";
-  programs.gpg.scdaemonSettings.reader-port = "Yubico Yubi";
 
   programs.ssh = {
     matchBlocks = {
