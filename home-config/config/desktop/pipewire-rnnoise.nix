b9870fbb4e8034e969f28bde35be56d82d20f44d
Tristan Daniël Maat <tristan.maat@codethink.co.uk>
2022-11-13 05:23:34 +0000

    pipewire: Move rnnoise configuration to in-line json

--- home-config/config/desktop/pipewire-rnnoise.nix
+++ home-config/config/desktop/pipewire-rnnoise.nix
@@ -5,9 +5,55 @@
   ...
 }: let
   inherit (pkgs) rnnoise-plugin pipewire;
-  pipewire-rnnoise-conf =
-    builtins.replaceStrings ["\${rnnoise-plugin}"] ["${rnnoise-plugin}"]
-    (builtins.readFile "${config._dotfiles}/filter-chain.conf");
+  pipewire-rnnoise-conf = pkgs.writeText "pipewire-rnnoise.conf" (builtins.toJSON {
+    "context.properties" = {
+      "log.level" = 2;
+    };
+
+    "context.spa-libs" = {
+      "audio.convert.*" = "audioconvert/libspa-audioconvert";
+      "support.*" = "support/libspa-support";
+    };
+
+    "context.modules" = [
+      {
+        name = "libpipewire-module-rtkit";
+        args = {};
+        flags = ["ifexists" "nofail"];
+      }
+      {name = "libpipewire-module-protocol-native";}
+      {name = "libpipewire-module-client-node";}
+      {name = "libpipewire-module-adapter";}
+      {
+        name = "libpipewire-module-filter-chain";
+        args = {
+          "node.name" = "rnnoise_source";
+          "node.description" = "Noise Canceling source";
+          "media.name" = "Noise Canceling source";
+          "filter.graph" = {
+            nodes = [
+              {
+                type = "ladspa";
+                name = "rnnoise";
+                plugin = "${rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
+                label = "noise_suppressor_stereo";
+                control = {
+                  "VAD Threshold (%)" = 80.0;
+                };
+              }
+            ];
+          };
+
+          "capture.props" = {
+            "node.passive" = true;
+          };
+          "playback.props" = {
+            "media.class" = "Audio/Source";
+          };
+        };
+      }
+    ];
+  });
 in {
   config = lib.mkIf config.custom.desktop-environment {
     systemd.user.services = {
@@ -18,9 +64,7 @@ in {
           Description = "rnnoise plugin for pipewire";
         };
         Service = {
-          ExecStart = "${pipewire}/bin/pipewire -c ${
-            pkgs.writeText "pipewire-rnnoise.conf" pipewire-rnnoise-conf
-          }";
+          ExecStart = "${pipewire}/bin/pipewire -c ${pipewire-rnnoise-conf}";
         };
         Install = {WantedBy = ["pipewire.service"];};
       };
