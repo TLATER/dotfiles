1ef1aa27e7192f628c1ce0d5bfacfc2ed55869e0
Tristan Daniël Maat <tm@tlater.net>
2023-04-11 21:46:12 +0100

    hyprland: Add desktop background

--- home-config/config/desktop/hyprland.nix
+++ home-config/config/desktop/hyprland.nix
@@ -2,8 +2,20 @@
   config,
   lib,
   pkgs,
+  flake-inputs,
   ...
-}: {
+}: let
+  wpaperd-config = {
+    default = {
+      path = "~/Documents/Pictures/Backgrounds";
+    };
+  };
+
+  wpaperd-config-dir = pkgs.runCommand "wpaperd-config" {} ''
+    mkdir -p $out/wpaperd
+    cp ${(pkgs.formats.toml {}).generate "wallpaper.toml" wpaperd-config} $out/wpaperd/wallpaper.toml
+  '';
+in {
   config = lib.mkIf config.custom.desktop-environment {
     home.packages = with pkgs; [
       slurp
@@ -16,6 +28,21 @@
       nvidiaPatches = true;
 
       extraConfig = builtins.readFile ../../dotfiles/hyprland.conf;
+    };
+
+    systemd.user.services.wpaperd = {
+      Unit = {
+        Description = "Wallpaper daemon";
+        After = ["graphical-session-pre.target"];
+        PartOf = ["graphical-session.target"];
+      };
+
+      Service = {
+        ExecStart = "${flake-inputs.nixpkgs-unstable.legacyPackages.${pkgs.system}.wpaperd}/bin/wpaperd --no-daemon";
+        Environment = "XDG_CONFIG_HOME=${wpaperd-config-dir}";
+      };
+
+      Install.WantedBy = ["graphical-session.target"];
     };
   };
 }
