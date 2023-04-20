92deaf906d5220303793ef9e3f0b565b76e435b9
Tristan Daniël Maat <tm@tlater.net>
2023-04-21 00:28:06 +0100

    NixOS: Turn on store auto-optimization

--- nixos-config/default.nix
+++ nixos-config/default.nix
@@ -16,11 +16,10 @@
 
   nix = {
     package = pkgs.nixFlakes;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
 
     settings = {
+      auto-optimise-store = true;
+      experimental-features = ["nix-command" "flakes"];
       substituters = ["https://hyprland.cachix.org"];
       trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
