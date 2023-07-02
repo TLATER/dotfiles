9109a405fa6bc948a1f3b02d29efae127a5818a1
Tristan Daniël Maat <tm@tlater.net>
2023-07-02 13:37:10 +0100

    yui: Split out games config and install steam natively

---
+++ nixos-config/yui/games.nix
@@ -0,0 +1,30 @@
+{
+  programs.steam.enable = true;
+
+  # Make steam controller work
+  hardware.steam-hardware.enable = true;
+  services.joycond.enable = true;
+
+  # This sets up gamescope as an independent compositor through a
+  # .desktop file.
+  #
+  # TODO(tlater): Figure out how to make this actually usable from
+  # greetd.
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
+    args = [
+      "--steam"
+      "--expose-wayland"
+      "--rt"
+      "-W 1920"
+      "-H 1080"
+      "--force-grab-cursor"
+      "--grab"
+      "--fullscreen"
+    ];
+  };
+
+  # Star citizen needs more
+  boot.kernel.sysctl."vm.max_map_count" = 16777216;
+}
