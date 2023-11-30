fdbf9107e25dff4ee8762da969b1e4adf091f25e
Tristan Daniël Maat <tm@tlater.net>
2023-11-30 14:15:35 +0100

    xdg-desktop-portal-wlr: Add screen selection for screencast

--- nixos-config/sway.nix
+++ nixos-config/sway.nix
@@ -29,6 +29,14 @@
 
   xdg.portal = {
     enable = true;
-    wlr.enable = true;
+    wlr = {
+      enable = true;
+      settings.screencast = {
+        output_name = "DP-1";
+        max_fps = 30;
+        chooser_type = "simple";
+        chooser_cmd = "${pkgs.slurp}/bin/slurp -f %o -or -s '#99d1ce33'";
+      };
+    };
   };
 }
