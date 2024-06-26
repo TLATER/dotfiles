6d9e9587d090ec6db1c2fa9c9d528a280ba628dc
Tristan Daniël Maat <tm@tlater.net>
2024-06-26 22:50:02 +0200

    yui: Work around simpledrm issues
    See https://github.com/NixOS/nixpkgs/issues/302059 for details

--- nixos-config/hosts/yui/nvidia/default.nix
+++ nixos-config/hosts/yui/nvidia/default.nix
@@ -24,23 +24,29 @@
     };
   };
 
-  boot.extraModprobeConfig =
-    "options nvidia "
-    + lib.concatStringsSep " " [
-      # nvidia assume that by default your CPU does not support PAT,
-      # but this is effectively never the case in 2023
-      "NVreg_UsePageAttributeTable=1"
-      # This may be a noop, but it's somewhat uncertain
-      "NVreg_EnablePCIeGen3=1"
-      # This is sometimes needed for ddc/ci support, see
-      # https://www.ddcutil.com/nvidia/
-      #
-      # Current monitor does not support it, but this is useful for
-      # the future
-      "NVreg_RegistryDwords=RMUseSwI2c=0x01;RMI2cSpeed=100"
-      # When (if!) I get another nvidia GPU, check for resizeable bar
-      # settings
+  boot = {
+    kernelParams = [
+      "nvidia-drm.fbdev=1"
     ];
+
+    extraModprobeConfig =
+      "options nvidia "
+      + lib.concatStringsSep " " [
+        # nvidia assume that by default your CPU does not support PAT,
+        # but this is effectively never the case in 2023
+        "NVreg_UsePageAttributeTable=1"
+        # This may be a noop, but it's somewhat uncertain
+        "NVreg_EnablePCIeGen3=1"
+        # This is sometimes needed for ddc/ci support, see
+        # https://www.ddcutil.com/nvidia/
+        #
+        # Current monitor does not support it, but this is useful for
+        # the future
+        "NVreg_RegistryDwords=RMUseSwI2c=0x01;RMI2cSpeed=100"
+        # When (if!) I get another nvidia GPU, check for resizeable bar
+        # settings
+      ];
+  };
 
   environment.variables = {
     # Required to run the correct GBM backend for nvidia GPUs on wayland
