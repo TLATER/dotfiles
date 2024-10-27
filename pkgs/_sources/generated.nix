ef26378a44f3d930394918fab110a45d257be5a6
Tristan Daniël Maat <tm@tlater.net>
2024-10-27 15:29:07 +0100

    fix: Downgrade gcs

--- pkgs/_sources/generated.nix
+++ pkgs/_sources/generated.nix
@@ -99,13 +99,13 @@
   };
   gcs = {
     pname = "gcs";
-    version = "v5.28.0";
+    version = "v5.27.0";
     src = fetchFromGitHub {
       owner = "richardwilkes";
       repo = "gcs";
-      rev = "v5.28.0";
+      rev = "v5.27.0";
       fetchSubmodules = false;
-      sha256 = "sha256-VfWRLfyK4RMF16ck5p+dXkMnrMjJw7DSlQvPMT1By+c=";
+      sha256 = "sha256-N4a+7a1XpDXsZTDiTh1AZIzXfk/A+gV4uBZdGZh+S5I=";
     };
   };
   ohmyzsh = {
