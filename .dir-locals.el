cc39066687cf7d5c142012da47ed367a89cf54d0
Tristan Daniël Maat <tm@tlater.net>
2025-01-05 17:08:41 +0800

    WIP: feat(eww): Add bluetooth indicator

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,10 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
 ((emacs-lisp-mode . ((fill-column . 88)
                      (indent-tabs-mode . nil)
                      (elisp-lint-indent-specs . ((use-package . 1)
-                                                 (reformatter-define . 1)))
-                     )))
+                                                 (reformatter-define . 1)))))
+ (rust-mode
+  . ((eglot-workspace-configuration
+      . (:rust-analyzer (:linkedProjects ["./home-config/dotfiles/eww/utils/Cargo.toml"]))))))
