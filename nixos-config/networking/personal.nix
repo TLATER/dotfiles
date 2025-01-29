c9bf3249ff76b3c8d7b001cd2a62f1597fe4723c
Tristan Daniël Maat <tm@tlater.net>
2025-01-29 21:39:59 +0800

    feat(networking): Switch to nftables

--- nixos-config/networking/personal.nix
+++ nixos-config/networking/personal.nix
@@ -1,61 +1,65 @@
 { config, ... }:
 {
-  networking.networkmanager = {
-    ensureProfiles.environmentFiles = [ config.sops.secrets.wireless-env.path ];
+  networking = {
+    nftables.enable = true;
 
-    ensureProfiles.profiles = {
-      bond = {
-        connection = {
-          id = "bond";
-          type = "bond";
-          interface-name = "bond0";
-        };
+    networkmanager = {
+      ensureProfiles.environmentFiles = [ config.sops.secrets.wireless-env.path ];
 
+      ensureProfiles.profiles = {
         bond = {
-          miimon = 100;
-          mode = "active-backup";
-          primary_reselect = "always";
-          fail_over_mac = "active";
-          updelay = 200;
+          connection = {
+            id = "bond";
+            type = "bond";
+            interface-name = "bond0";
+          };
+
+          bond = {
+            miimon = 100;
+            mode = "active-backup";
+            primary_reselect = "always";
+            fail_over_mac = "active";
+            updelay = 200;
+          };
+
+          ipv4.method = "auto";
+          ipv6 = {
+            addr-gen-mode = "default";
+            method = "auto";
+          };
         };
 
-        ipv4.method = "auto";
-        ipv6 = {
-          addr-gen-mode = "default";
-          method = "auto";
-        };
-      };
+        ethernet = {
+          connection = {
+            id = "ethernet";
+            type = "ethernet";
 
-      ethernet = {
-        connection = {
-          id = "ethernet";
-          type = "ethernet";
-
-          master = "bond0";
-          slave-type = "bond";
-        };
-      };
-
-      mikan = {
-        connection = {
-          id = "mikan";
-          type = "wifi";
-
-          master = "bond0";
-          slave-type = "bond";
-
-          autoconnect = true;
-          autoconnect-priority = 100;
+            master = "bond0";
+            slave-type = "bond";
+          };
         };
 
-        wifi = {
-          mode = "infrastructure";
-          ssid = "mikan";
-        };
+        mikan = {
+          connection = {
+            id = "mikan";
+            type = "wifi";
 
-        wifi-security = {
-          key-mgmt = "wpa-psk";
-          psk = "$PSK_MIKAN";
+            master = "bond0";
+            slave-type = "bond";
+
+            autoconnect = true;
+            autoconnect-priority = 100;
+          };
+
+          wifi = {
+            mode = "infrastructure";
+            ssid = "mikan";
+          };
+
+          wifi-security = {
+            key-mgmt = "wpa-psk";
+            psk = "$PSK_MIKAN";
+          };
         };
       };
     };
