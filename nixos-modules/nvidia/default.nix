1730f18697937a0ea3b238264ecf1ee0d7eed546
Tristan Daniël Maat <tm@tlater.net>
2025-03-01 02:34:35 +0800

    bump(nvidia): Update nvidia driver

--- nixos-modules/nvidia/default.nix
+++ nixos-modules/nvidia/default.nix
@@ -29,11 +29,11 @@ in
 
     hardware.nvidia = {
       package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-        version = "565.77";
-        sha256_64bit = "sha256-CnqnQsRrzzTXZpgkAtF7PbH9s7wbiTRNcM0SPByzFHw=";
+        version = "570.124.04";
+        sha256_64bit = "sha256-G3hqS3Ei18QhbFiuQAdoik93jBlsFI2RkWOBXuENU8Q=";
         sha256_aarch64 = lib.fakeHash;
-        openSha256 = "sha256-Fxo0t61KQDs71YA8u7arY+503wkAc1foaa51vi2Pl5I=";
-        settingsSha256 = "sha256-VUetj3LlOSz/LB+DDfMCN34uA4bNTTpjDrb6C6Iwukk=";
+        openSha256 = "sha256-KCGUyu/XtmgcBqJ8NLw/iXlaqB9/exg51KFx0Ta5ip0=";
+        settingsSha256 = "sha256-LNL0J/sYHD8vagkV1w8tb52gMtzj/F0QmJTV1cMaso8=";
         persistencedSha256 = lib.fakeHash;
       };
 
