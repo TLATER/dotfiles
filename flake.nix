663cc95847d343835ec9b121ca3a2e8a59a50371
Tristan Daniël Maat <t.maat@famedly.com>
2025-03-27 22:34:55 +0800

    feat(git): Set up mergiraf

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     # NixOS related inputs
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     sops-nix = {
--- home-config/config/applications/tty/default.nix
+++ home-config/config/applications/tty/default.nix
@@ -1,8 +1,39 @@
 {
+  pkgs,
+  lib,
+  flake-inputs,
+  ...
+}:
+{
   imports = [ ./emacs.nix ];
 
   programs.bottom = {
     enable = true;
     settings.flags.group_processes = true;
   };
+
+  programs.git.extraConfig =
+    let
+      # Using unstable because version 0.6.0 introduced an
+      # important fix to the gitattributes format
+      inherit (flake-inputs.nixpkgs-unstable.legacyPackages.${pkgs.system}) mergiraf;
+      mergiraf-attributes =
+        pkgs.runCommandLocal "gitattributes"
+          {
+            nativeBuildInputs = [
+              mergiraf
+            ];
+          }
+          ''
+            mergiraf languages --gitattributes >> $out
+          '';
+    in
+    {
+      core.attributesfile = mergiraf-attributes.outPath;
+
+      merge.mergiraf = {
+        name = "mergiraf";
+        driver = "${lib.getExe mergiraf} merge --git %O %A %B -s %S -x %X -y %Y -p %P -l %L";
+      };
+    };
 }
