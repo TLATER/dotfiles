ae13176ddbadf212964b5def109e806405c0ac96
Tristan Daniël Maat <tm@tlater.net>
2025-04-28 00:39:20 +0800

    update(nvidia): 570.124.04 -> 570.133.07
    Security relevant: https://www.gamingonlinux.com/2025/04/nvidia-disclose-new-security-flaw-in-their-linux-gpu-drivers/

--- nixos-modules/nvidia/default.nix
+++ nixos-modules/nvidia/default.nix
@@ -32,11 +32,11 @@ in
 
     hardware.nvidia = {
       package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-        version = "570.124.04";
-        sha256_64bit = "sha256-G3hqS3Ei18QhbFiuQAdoik93jBlsFI2RkWOBXuENU8Q=";
+        version = "570.133.07";
+        sha256_64bit = "sha256-LUPmTFgb5e9VTemIixqpADfvbUX1QoTT2dztwI3E3CY=";
         sha256_aarch64 = lib.fakeHash;
-        openSha256 = "sha256-KCGUyu/XtmgcBqJ8NLw/iXlaqB9/exg51KFx0Ta5ip0=";
-        settingsSha256 = "sha256-LNL0J/sYHD8vagkV1w8tb52gMtzj/F0QmJTV1cMaso8=";
+        openSha256 = "sha256-9l8N83Spj0MccA8+8R1uqiXBS0Ag4JrLPjrU3TaXHnM=";
+        settingsSha256 = "sha256-XMk+FvTlGpMquM8aE8kgYK2PIEszUZD2+Zmj2OpYrzU=";
         persistencedSha256 = lib.fakeHash;
       };
 
