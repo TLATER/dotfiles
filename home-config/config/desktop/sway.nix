fc9aaf03aade8f11c0b991c818b3cd96aa8485e5
Tristan Daniël Maat <tm@tlater.net>
2025-05-08 21:35:48 +0800

    refactor(wpaperd): Use the new config file flag

--- home-config/config/desktop/sway.nix
+++ home-config/config/desktop/sway.nix
@@ -10,16 +10,9 @@ let
   swaymsg = "${nixos-config.programs.sway.package or pkgs.sway}/bin/swaymsg";
   systemctl = "${pkgs.systemd}/bin/systemctl";
 
-  wpaperd-config = {
-    default = {
-      path = "~/Documents/Pictures/Backgrounds";
-    };
+  wpaperd-config = (pkgs.formats.toml { }).generate "wallpaper.toml" {
+    default.path = "~/Documents/Pictures/Backgrounds";
   };
-
-  wpaperd-config-dir = pkgs.runCommand "wpaperd-config" { } ''
-    mkdir -p $out/wpaperd
-    cp ${(pkgs.formats.toml { }).generate "wallpaper.toml" wpaperd-config} $out/wpaperd/wallpaper.toml
-  '';
 
   keepassxc-copy = pkgs.writeShellApplication {
     name = "keepassxc-copy";
@@ -123,12 +116,10 @@ in
         Description = "Wallpaper daemon";
         After = [ "graphical-session-pre.target" ];
         PartOf = [ "graphical-session.target" ];
+        X-Restart-Triggers = [ wpaperd-config ];
       };
 
-      Service = {
-        ExecStart = "${pkgs.wpaperd}/bin/wpaperd";
-        Environment = "XDG_CONFIG_HOME=${wpaperd-config-dir}";
-      };
+      Service.ExecStart = "${pkgs.wpaperd}/bin/wpaperd -c ${wpaperd-config}";
 
       Install.WantedBy = [ "graphical-session.target" ];
     };
