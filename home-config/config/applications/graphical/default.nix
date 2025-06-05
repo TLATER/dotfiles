36f4113bef3a243d4635cc0793d09e8e938b1119
Tristan Daniël Maat <tm@tlater.net>
2025-06-05 20:27:38 +0800

    feat(alacritty): Switch to oksolar

--- home-config/config/applications/graphical/default.nix
+++ home-config/config/applications/graphical/default.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   imports = [
     ./discord.nix
@@ -9,12 +14,57 @@
   ];
 
   home.packages = with pkgs; [
-    alacritty
     apvlv
     feh
     xsel
     yubioath-flutter
   ];
 
-  xdg.configFile."alacritty/alacritty.toml".source = "${config._dotfiles}/alacritty/alacritty.toml";
+  programs.alacritty =
+    let
+      inherit (config.lib.stylix.colors.withHashtag)
+        base10
+        base11
+        base01
+        base02
+        base04
+        base06
+        ;
+    in
+    {
+      enable = true;
+      # We don't want stylix to apply its fonts, we use fontconfig for
+      # that
+      settings = {
+        font = lib.mkForce { };
+
+        colors.indexed_colors = [
+          {
+            index = 16;
+            color = base11;
+          }
+          {
+            index = 17;
+            color = base10;
+          }
+          {
+            index = 18;
+            color = base01;
+          }
+          {
+            index = 19;
+            color = base02;
+          }
+          {
+            index = 20;
+            color = base04;
+          }
+          {
+            index = 21;
+            color = base06;
+          }
+        ];
+      };
+    };
+  stylix.targets.alacritty.enable = true;
 }
