f29d9707c119c451b86f465dc0b84163b3b1d4f6
Tristan Daniël Maat <tm@tlater.net>
2025-08-14 03:26:51 +0800

    feat(games): Actually use nix-gaming modules

--- nixos-config/hosts/yui/games.nix
+++ nixos-config/hosts/yui/games.nix
@@ -1,16 +1,31 @@
-{ pkgs, ... }:
+{ flake-inputs, pkgs, ... }:
+let
+  inherit (flake-inputs) nix-gaming;
+in
 {
+  imports = [
+    nix-gaming.nixosModules.pipewireLowLatency
+    nix-gaming.nixosModules.platformOptimizations
+  ];
+
   environment.systemPackages = [ pkgs.mangohud ];
 
   # Make steam controller work
   hardware.steam-hardware.enable = true;
-  services.joycond.enable = true;
+
+  services = {
+    joycond.enable = true;
+    pipewire.lowLatency.enable = true;
+  };
 
   programs = {
     steam = {
       enable = true;
       extraCompatPackages = [ pkgs.proton-ge-bin ];
       gamescopeSession.enable = true;
+      # This sets some sensible game performance settings, along with
+      # some required for Star Citizen
+      platformOptimizations.enable = true;
     };
     gamescope = {
       enable = true;
@@ -27,7 +42,4 @@
       ];
     };
   };
-
-  # Star citizen needs more
-  boot.kernel.sysctl."vm.max_map_count" = 16777216;
 }
