db7df207eb5a199d379b82c2532bbb5d62a5acb6
Tristan Daniël Maat <tm@tlater.net>
2025-08-14 07:34:11 +0800

    feat(games): Remove EAC workaround
    This *should* no longer be necessary and doesn't work anymore anyway.

--- nixos-config/hosts/yui/networking.nix
+++ nixos-config/hosts/yui/networking.nix
@@ -9,9 +9,6 @@
     ];
 
     firewall.allowedUDPPorts = [ 25565 ];
-
-    # Work around EAC
-    hosts."127.0.0.1" = [ "modules-cdn.eac-prod.on.epicgames.com" ];
   };
 
   networking.networkmanager.ensureProfiles.profiles.bond.bond.primary = "eno1";
