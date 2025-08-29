7afdc932b7aa30d774e1b558f739c316b9d0b14f
Tristan Daniël Maat <tm@tlater.net>
2025-08-30 07:33:28 +0800

    feat(networking): Harden unbound a bit further

--- nixos-config/networking/default.nix
+++ nixos-config/networking/default.nix
@@ -11,8 +11,27 @@
 
     settings = {
       server = {
+        # Sends less identifying data for queries
         qname-minimisation = true;
+
+        # Prevent DNSSEC algorithm downgrades
+        harden-algo-downgrade = true;
+
+        # Required for DoT, unbound does not include the system (or
+        # any) certs by default
         tls-system-cert = true;
+
+        # These addresses are forbidden from public responses, but
+        # unbound does not enforce this by default
+        private-address = [
+          "10.0.0.0/8"
+          "172.16.0.0/12" # Maybe allow these because ad blockers use them
+          "192.168.0.0/16"
+          "169.254.0.0/16"
+          "fd00::/8"
+          "fe80::/10"
+          "::ffff:0:0/96"
+        ];
       };
 
       forward-zone = [
