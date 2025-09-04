16cfb6b924ec763d43be40359b4cc861fd9a0e4b
Tristan Daniël Maat <t.maat@famedly.com>
2025-09-04 13:55:21 +0800

    feat(networking/work): Add TGR wifi definition

--- nixos-config/networking/work.nix
+++ nixos-config/networking/work.nix
@@ -23,6 +23,34 @@
           psk = "$PSK_LALA_GUEST";
         };
       };
+
+      tgr = {
+        connection = {
+          id = "tgr";
+          type = "wifi";
+
+          autoconnect = true;
+          autoconnect-priority = 100;
+        };
+
+        wifi = {
+          mode = "infrastructure";
+          ssid = "The Great Room";
+          # Disable if we get issues after the transition on 21 September
+          # scan-rand-mac-address = false;
+        };
+
+        wifi-security = {
+          key-mgmt = "wpa-psk";
+          psk = "$PSK_TGR";
+        };
+
+        ipv4.method = "auto";
+        ipv6 = {
+          addr-gen-mode = "default";
+          method = "auto";
+        };
+      };
     };
   };
 
