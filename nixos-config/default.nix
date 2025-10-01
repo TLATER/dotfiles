c4e1099c002a0caa006dcd8d569aeff6d7e7c30b
Tristan Daniël Maat <tm@tlater.net>
2025-10-01 22:36:08 +0800

    chore: Remove nix-community cache
    I... don't remember why I added it in the first place.

--- nixos-config/default.nix
+++ nixos-config/default.nix
@@ -28,8 +28,6 @@
         "nix-command"
         "flakes"
       ];
-      substituters = [ "https://nix-community.cachix.org" ];
-      trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
     };
 
     gc = {
