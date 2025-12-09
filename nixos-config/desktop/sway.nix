92c1cc89a42fcc916ffc734a411cb0e895cd7d77
Tristan Daniël Maat <tm@tlater.net>
2025-12-09 20:57:37 +0800

    fix(sway): Fix screensharing
    See https://github.com/emersion/xdg-desktop-portal-wlr/issues/350

--- nixos-config/desktop/sway.nix
+++ nixos-config/desktop/sway.nix
@@ -14,12 +14,7 @@
     enable = true;
     wlr = {
       enable = true;
-      settings.screencast = {
-        output_name = "DP-1";
-        max_fps = 30;
-        chooser_type = "simple";
-        chooser_cmd = "${pkgs.slurp}/bin/slurp -f %o -or -s '#99d1ce33'";
-      };
+      settings.screencast.max_fps = 30;
     };
   };
 }
