ef5179ed4192583bf787e2ff144e7580add4094c
Tristan Daniël Maat <t.maat@famedly.com>
2026-03-13 17:46:15 +0800

    chore(work/networking): Remove mikan-guest

--- nixos-config/networking/work.nix
+++ nixos-config/networking/work.nix
@@ -23,34 +23,6 @@
           psk = "$PSK_LALA_GUEST";
         };
       };
-
-      mikan-guest = {
-        connection = {
-          id = "mikan-guest";
-          type = "wifi";
-
-          autoconnect = true;
-          autoconnect-priority = 100;
-          # Attempt to connect indefinitely
-          autoconnect-retries = 0;
-        };
-
-        wifi = {
-          mode = "infrastructure";
-          ssid = "mikan-guest";
-        };
-
-        wifi-security = {
-          key-mgmt = "sae";
-          psk = "$PSK_MIKAN_GUEST";
-        };
-
-        ipv4.method = "auto";
-        ipv6 = {
-          addr-gen-mode = "default";
-          method = "auto";
-        };
-      };
     };
   };
 
