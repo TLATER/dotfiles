487f75c4d0e85aaae3031c886cc81dc8ff587c97
Tristan Daniël Maat <tm@tlater.net>
2026-05-10 05:47:04 +0800

    style(rin): Fix formatting mistake

--- nixos-config/hosts/rin/default.nix
+++ nixos-config/hosts/rin/default.nix
@@ -40,27 +40,29 @@
 
   users.users.tlater.extraGroups = [ "docker" ];
 
-  # Incompatible with docker
-  networking.nftables.enable = lib.mkForce false;
+  networking = {
+    # Incompatible with docker
+    nftables.enable = lib.mkForce false;
 
-  # Allow docker containers to communicate
-  networking.firewall.extraCommands =
-    let
-      # Either get the docker daemon setting *or* the default value
-      dockerAddressPools =
-        config.virtualisation.docker.daemon.settings.default-address-pools or [
-          {
-            base = "172.30.0.0/16";
-            size = 24;
-          }
-          {
-            base = "172.31.0.0/16";
-            size = 24;
-          }
-        ];
-      addresses = lib.concatMapStringsSep "," (pool: pool.base) dockerAddressPools;
-    in
-    ''
-      iptables -A INPUT -s ${addresses} -d ${addresses},172.17.0.1 -j ACCEPT
-    '';
+    # Allow docker containers to communicate
+    firewall.extraCommands =
+      let
+        # Either get the docker daemon setting *or* the default value
+        dockerAddressPools =
+          config.virtualisation.docker.daemon.settings.default-address-pools or [
+            {
+              base = "172.30.0.0/16";
+              size = 24;
+            }
+            {
+              base = "172.31.0.0/16";
+              size = 24;
+            }
+          ];
+        addresses = lib.concatMapStringsSep "," (pool: pool.base) dockerAddressPools;
+      in
+      ''
+        iptables -A INPUT -s ${addresses} -d ${addresses},172.17.0.1 -j ACCEPT
+      '';
+  };
 }
