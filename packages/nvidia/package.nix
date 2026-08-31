1213c7f10040ccc36798b5097cd7144fedde0e8f
Tristan Daniël Maat <tm@tlater.net>
2026-08-31 18:13:36 +0800

    update(pkgs): Update sources of all downstream packages

--- packages/nvidia/package.nix
+++ packages/nvidia/package.nix
@@ -10,9 +10,9 @@ let
   inherit (inputs.self.nixosConfigurations.yui.config.boot) kernelPackages;
 in
 (kernelPackages.nvidiaPackages.mkDriver {
-  version = "595.91.07";
-  sha256_64bit = "sha256-yiPIjdJLB6GRZE4eEc+3vN11NzBXSa9A+YABiwleYxM=";
-  openSha256 = "sha256-OB8Epd+qn/WywxsPiFpxEOAzlJqb6I1SyRoV3a8l71k=";
+  version = "595.99.02";
+  sha256_64bit = "sha256-6HR3lYv3YwcFSTJL1a1slI66btIQ5EAFs+/4SUD24ew=";
+  openSha256 = "sha256-T36x/jx8yQ8l3LFp1rZIrTfcSwbGy8YSAvXOUSptpb4=";
 
   useSettings = false;
   usePersistenced = false;
